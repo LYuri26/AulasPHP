546ba6fcd6067babfbc3ce8ca8946dd69dc7b45f
Lenon <lenonyurisilva26@gmail.com>
2023-11-23 10:48:31 -0300

    23/11/23

--- Hospedagem/DataBase/conexao.php
+++ Hospedagem/DataBase/conexao.php
@@ -6,61 +6,62 @@ $dbname = "hospedagemypua";
 $username = "root";
 $password = "";
 
-// Conecta ao banco de dados
-try {
-  $conexao = new PDO("mysql:host=$host", $username, $password);
-  $conexao->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-} catch (PDOException $e) {
-  die("Erro ao conectar ao banco de dados: " . $e->getMessage());
+// Função para conectar ao banco de dados e retornar a conexão
+function conexao() {
+    global $host, $dbname, $username, $password;
+    
+    try {
+        $conexao = new PDO("mysql:host=$host", $username, $password);
+        $conexao->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        
+        // Verifica se o banco de dados existe
+        $query = "SELECT SCHEMA_NAME FROM INFORMATION_SCHEMA.SCHEMATA WHERE SCHEMA_NAME = '$dbname';";
+        $resultado = $conexao->query($query);
+
+        // Se o banco de dados não existir, cria ele
+        if ($resultado->rowCount() === 0) {
+            $conexao->exec("CREATE DATABASE $dbname;");
+        }
+
+        // Conecta ao banco de dados especificado
+        $conexao->exec("USE $dbname;");
+
+        // Cria as tabelas se elas não existirem
+        $queryUsuarios = "CREATE TABLE IF NOT EXISTS usuarios (
+            id INT AUTO_INCREMENT PRIMARY KEY,
+            nome VARCHAR(255) NOT NULL,
+            email VARCHAR(255) NOT NULL UNIQUE,
+            senha VARCHAR(255) NOT NULL,
+            perfil VARCHAR(255) NOT NULL
+        );";
+        $conexao->exec($queryUsuarios);
+
+        $queryHospedagens = "CREATE TABLE IF NOT EXISTS hospedagens (
+            id INT AUTO_INCREMENT PRIMARY KEY,
+            data_check_in DATE NOT NULL,
+            data_check_out DATE NOT NULL,
+            numero_de_hospedes INT NOT NULL,
+            tipo_de_acomodacao VARCHAR(255) NOT NULL,
+            cliente_id INT NOT NULL
+        );";
+        $conexao->exec($queryHospedagens);
+
+        $queryFuncionarios = "CREATE TABLE IF NOT EXISTS funcionarios (
+            id INT AUTO_INCREMENT PRIMARY KEY,
+            nome VARCHAR(255) NOT NULL,
+            email VARCHAR(255) NOT NULL UNIQUE,
+            senha VARCHAR(255) NOT NULL,
+            cargo VARCHAR(255) NOT NULL
+        );";
+        $conexao->exec($queryFuncionarios);
+
+        return $conexao;
+    } catch (PDOException $e) {
+        die("Erro ao conectar ao banco de dados: " . $e->getMessage());
+    }
 }
-
-// Verifica se o banco de dados existe
-$query = "SELECT SCHEMA_NAME FROM INFORMATION_SCHEMA.SCHEMATA WHERE SCHEMA_NAME = 'hospedagemypua';";
-$resultado = $conexao->query($query);
-
-// Se o banco de dados não existir, cria ele
-if ($resultado->rowCount() === 0) {
-  $query = "CREATE DATABASE hospedagemypua;";
-  $conexao->exec($query);
-}
-
-// Conecta ao banco de dados
-$conexao->exec("USE hospedagemypua;");
-
-// Cria as tabelas
-
-// Cria a tabela Usuarios
-$query = "CREATE TABLE IF NOT EXISTS usuarios (
-  id INT AUTO_INCREMENT PRIMARY KEY,
-  nome VARCHAR(255) NOT NULL,
-  email VARCHAR(255) NOT NULL UNIQUE,
-  senha VARCHAR(255) NOT NULL,
-  perfil VARCHAR(255) NOT NULL
-);";
-$conexao->exec($query);
-
-// Cria a tabela Hospedagens
-$query = "CREATE TABLE IF NOT EXISTS hospedagens (
-  id INT AUTO_INCREMENT PRIMARY KEY,
-  data_check_in DATE NOT NULL,
-  data_check_out DATE NOT NULL,
-  numero_de_hospedes INT NOT NULL,
-  tipo_de_acomodacao VARCHAR(255) NOT NULL,
-  cliente_id INT NOT NULL
-);";
-$conexao->exec($query);
-
-// Cria a tabela Funcionarios
-$query = "CREATE TABLE IF NOT EXISTS funcionarios (
-  id INT AUTO_INCREMENT PRIMARY KEY,
-  nome VARCHAR(255) NOT NULL,
-  email VARCHAR(255) NOT NULL UNIQUE,
-  senha VARCHAR(255) NOT NULL,
-  cargo VARCHAR(255) NOT NULL
-);";
-$conexao->exec($query);
 
 // Retorna a conexão
-return $conexao;
+return conexao();
 
 ?>
--- Hospedagem/index.php
+++ Hospedagem/index.php
@@ -1,75 +1,81 @@
 <?php
+session_start();
 
 require_once './DataBase/conexao.php';
 
-// Verifica se o usuário está logado
-if (isset($_SESSION['usuario'])) {
-  // O usuário está logado, redireciona para a página inicial
-  header("Location: index.php");
-  exit;
-}
+$erro = '';
 
-// O usuário não está logado, carrega a tela de login
+// Verifica se houve envio do formulário
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    // Verifica se os campos foram preenchidos e não estão vazios
+    if (!empty($_POST['email']) && !empty($_POST['senha'])) {
+        $email = $_POST['email'];
+        $senha = md5($_POST['senha']); // Considerando que a senha no banco está criptografada com MD5
+        
+        // Conecta-se ao banco de dados
+        $conexao = conexao();
+
+        // Verifica as credenciais no banco de dados e busca o nome do usuário
+        $query = $conexao->prepare("SELECT nome FROM usuarios WHERE email = :email AND senha = :senha");
+        $query->bindParam(":email", $email);
+        $query->bindParam(":senha", $senha);
+        $query->execute();
+
+        // Verifica se encontrou um usuário correspondente
+        if ($query->rowCount() === 1) {
+            // Obtém o nome do usuário
+            $resultado = $query->fetch(PDO::FETCH_ASSOC);
+            $nomeUsuario = $resultado['nome'];
+
+            // Usuário autenticado, guarda o nome na sessão e redireciona para menu.php
+            $_SESSION['usuario'] = $nomeUsuario;
+            header("Location: menu.php");
+            exit;
+        } else {
+            // Credenciais inválidas, exibe uma mensagem de erro
+            $erro = "Credenciais inválidas. Por favor, verifique seu e-mail e senha.";
+        }
+    } else {
+        $erro = "Preencha todos os campos do formulário.";
+    }
+}
 ?>
 
 <!DOCTYPE html>
 <html lang="pt-br">
 <head>
-  <title>Login - Pousada Quinta do Ypuã</title>
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
+    <title>Login - Pousada Quinta do Ypuã</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
 </head>
 <body>
 
-  <div class="container">
-
+<div class="container">
     <h1>Login</h1>
 
-    <form action="index.php" method="post">
-      <div class="form-group">
-        <label for="email">E-mail</label>
-        <input type="email" class="form-control" id="email" name="email" placeholder="Digite seu e-mail">
-      </div>
-      <div class="form-group">
-        <label for="senha">Senha</label>
-        <input type="password" class="form-control" id="senha" name="senha" placeholder="Digite sua senha">
-      </div>
-      <button type="submit" class="btn btn-primary">Entrar</button>
+    <?php if (!empty($erro)): ?>
+        <div class="alert alert-danger" role="alert">
+            <?php echo $erro; ?>
+        </div>
+    <?php endif; ?>
+
+    <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
+        <div class="form-group">
+            <label for="email">E-mail</label>
+            <input type="email" class="form-control" id="email" name="email" placeholder="Digite seu e-mail">
+        </div>
+        <div class="form-group">
+            <label for="senha">Senha</label>
+            <input type="password" class="form-control" id="senha" name="senha" placeholder="Digite sua senha">
+        </div>
+        <button type="submit" class="btn btn-primary">Entrar</button>
     </form>
 
-  </div>
+    <div class="mt-3">
+        <a href="./Cadastro/cadastro.php" class="btn btn-secondary">Criar Cadastro</a>
+        <a href="recuperar_senha.php" class="btn btn-secondary">Recuperar Senha</a>
+    </div>
+
+</div>
 
 </body>
 </html>
-
-<?php
-
-// Verifica se os dados do formulário estão preenchidos
-if (empty($_POST['email']) || empty($_POST['senha'])) {
-  echo "Preencha todos os campos do formulário.";
-  exit;
-}
-
-// Conecta ao banco de dados
-$conexao = conexao();
-
-// Seleciona o usuário
-$query = $conexao->prepare("SELECT * FROM usuarios WHERE email = :email AND senha = :senha");
-$query->bindParam(":email", $_POST['email']);
-$query->bindParam(":senha", md5($_POST['senha']));
-$query->execute();
-
-// Verifica se o usuário foi encontrado
-if ($query->rowCount() === 0) {
-  echo "Usuário ou senha inválidos.";
-  exit;
-}
-
-// Cria a sessão do usuário
-$usuario = $query->fetch(PDO::FETCH_ASSOC);
-session_start();
-$_SESSION['usuario'] = $usuario;
-
-// Redireciona o usuário para a página inicial
-header("Location: index.php");
-
-?>
