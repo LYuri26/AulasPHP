b9184a0425a932c886f666424ef307f88e36a6d1
Lenon Yuri <lenonyurisilva26@gmail.com>
2025-04-24 21:34:38 -0300

    24/04/25

--- RetailHub/public/pages/clientes.php
+++ RetailHub/public/pages/clientes.php
@@ -28,7 +28,7 @@ $clientes = $query->fetchAll(PDO::FETCH_ASSOC);
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Cadastro de Clientes</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
-    <link href="css/style.css" rel="stylesheet">
+    <link href="../css/estilo.css" rel="stylesheet">
 </head>
 
 <body>
--- RetailHub/public/pages/produtos.php
+++ RetailHub/public/pages/produtos.php
@@ -31,7 +31,7 @@ $produtos = $query->fetchAll(PDO::FETCH_ASSOC);
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Cadastro de Produtos</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
-    <link href="./css/produtos.css" rel="stylesheet">
+    <link href="../css/produtos.css" rel="stylesheet">
 </head>
 
 <body>
